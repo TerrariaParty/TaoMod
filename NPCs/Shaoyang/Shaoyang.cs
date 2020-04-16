f964c0ba7337945a7a1216271d395c8a6e2d7f3d
Jaden Zheng <4567jaden@gmail.com>
2020-04-16 17:35:49 -0400

    Fixed shaoyang....?

--- NPCs/Shaoyang/Shaoyang.cs
+++ NPCs/Shaoyang/Shaoyang.cs
@@ -84,80 +84,105 @@ namespace TaoMod.NPCs.Shaoyang
 		private int LightTrailTimer = 0;
 		private int Phase1ChaseSpeed = 4;
 		private int FunnyExplosionOrbTimer = 0;
-		private const int AI_State_Slot = 0;
-		private const int State_Dash = 0;
-		private const int State_Orb = 1;
-		private const int State_Spear = 2;
-		private int AI_Timer = 0;
-		public float AI_State {
-			get => npc.ai[AI_State_Slot];
-			set => npc.ai[AI_State_Slot] = value;
-		}
-		public void ExpertOrbAttack()
+		private int DoOrb = 0;
+		private int DoSpear = 0;			
+		public override void AI()
 		{
-				npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * Phase1ChaseSpeed;
-				FunnyExplosionOrbTimer++;
-				if(FunnyExplosionOrbTimer % 150 == 0){
-					float numberProjectiles = 12;
-					float rotation = MathHelper.ToRadians(360);
-					for (int i = 0; i < numberProjectiles; i++)
-					{
-					Vector2 perturbedSpeed = new Vector2(2, 2).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
-					Projectile.NewProjectile(npc.Center, perturbedSpeed, ProjectileType<YangOrb>(), 35, 0f);
-					}
-					FunnyExplosionOrbTimer = 0;
-				}
+			npc.rotation++;
+			npc.TargetClosest(true);
+			if (++AlphaTimer % 3 == 0 && npc.alpha > 40)
+			{
+				npc.alpha -= 5;
+				AlphaTimer = 0;
 			}
 
-		public void NormalOrbAttack()
-		{
-				npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * Phase1ChaseSpeed;
-				FunnyExplosionOrbTimer++;
-				if(FunnyExplosionOrbTimer % 225 == 0){
-					float numberProjectiles = 6;
-					float rotation = MathHelper.ToRadians(360);
-					for (int i = 0; i < numberProjectiles; i++)
+			if (Main.expertMode)
+			{
+				if (npc.life > 0)
+				{
+					if (DoOrb % 100 == 0)
 					{
-					Vector2 perturbedSpeed = new Vector2(2, 2).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
-					Projectile.NewProjectile(npc.Center, perturbedSpeed, ProjectileType<YangOrb>(), 25, 0f);
+						npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * Phase1ChaseSpeed;
+						FunnyExplosionOrbTimer++;
+						if (FunnyExplosionOrbTimer % 150 == 0)
+						{
+							float numberProjectiles = 12;
+							float rotation = MathHelper.ToRadians(360);
+							for (int i = 0; i < numberProjectiles; i++)
+							{
+								Vector2 perturbedSpeed = new Vector2(2, 2).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
+								Projectile.NewProjectile(npc.Center, perturbedSpeed, ProjectileType<YangOrb>(), 35, 0f);
+							}
+							FunnyExplosionOrbTimer = 0;
+						}
+						DoOrb = 0;
 					}
-					FunnyExplosionOrbTimer = 0;
+					if (DashDuration > 0)
+						LightTrailTimer++;
+					}
+					if (LightTrailTimer >= 3)
+					{
+						Projectile.NewProjectile(npc.Center, lightTrailVelocity, ModContent.ProjectileType<LightTrail>(), 10, 0f);
+						LightTrailTimer = 0;
+					}
+					DashDuration--;
+					if (++DashTimer % 180 == 0)
+					{
+						if (DashDuration < 0)
+						{
+							Main.PlaySound(new LegacySoundStyle(SoundID.Roar, 0), npc.Center);
+							DashDuration = 100;
+							npc.DirectionTo(Main.player[npc.target].Center);
+							npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * dashVelocity;
+						}
+					}
+
+					if (DashTimer > 0 && DashDuration == 0)
+					{
+						npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * stillVelocity;
+					}
+					if (DoSpear % 300 == 0)
+					{
+						ShootSpear++;
+						ExpertShootSpear++;
+						if (++ShootSpear % 480 == 0)
+						{
+							if (++ExpertShootSpear % 4 == 0)
+							{
+								NPC.NewNPC((int)npc.Center.X, (int)npc.position.Y, ModContent.NPCType<YangSpear>());
+								NPC.NewNPC((int)npc.Center.X + 50, (int)npc.position.Y - 53, ModContent.NPCType<YangSpear>());
+								NPC.NewNPC((int)npc.Center.X - 50, (int)npc.position.Y - 53, ModContent.NPCType<YangSpear>());
+
+								ExpertShootSpear = 0;
+							}
+							ShootSpear = 0;
+						}
+						DoSpear = 0;
+					}
 				}
 			}
-
-		public void ExpertDashAttack()
-		{
-				if (DashDuration > 0)
-					LightTrailTimer++;
-
-				if (LightTrailTimer >= 3)
+			else
+			{
+				if (DoOrb % 120 == 0)
 				{
-					Projectile.NewProjectile(npc.Center, lightTrailVelocity, ModContent.ProjectileType<LightTrail>(), 10, 0f);
-					LightTrailTimer = 0;
-				}
-
-				DashDuration--;
-
-				if (++DashTimer % 180 == 0)
-				{
-					if (DashDuration < 0)
+					npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * Phase1ChaseSpeed;
+					FunnyExplosionOrbTimer++;
+					if (FunnyExplosionOrbTimer % 225 == 0)
 					{
-						Main.PlaySound(new LegacySoundStyle(SoundID.Roar, 0), npc.Center);
-						DashDuration = 100;
-						npc.DirectionTo(Main.player[npc.target].Center);
-						npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * dashVelocity;
+						float numberProjectiles = 6;
+						float rotation = MathHelper.ToRadians(360);
+						for (int i = 0; i < numberProjectiles; i++)
+						{
+							Vector2 perturbedSpeed = new Vector2(2, 2).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
+							Projectile.NewProjectile(npc.Center, perturbedSpeed, ProjectileType<YangOrb>(), 25, 0f);
+						}
+						FunnyExplosionOrbTimer = 0;
 					}
+					DoOrb = 0;
 				}
-
-				if (DashTimer > 0 && DashDuration == 0)
-					npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * stillVelocity;
-				}
-
-		public void NormalDashAttack()
-		{
-				if (DashDuration > 0)
+				if (DashDuration > 0){
 					LightTrailTimer++;
-
+				}
 				if (LightTrailTimer >= 3)
 				{
 					Projectile.NewProjectile(npc.Center, lightTrailVelocity, ModContent.ProjectileType<LightTrail>(), 10, 0f);
@@ -178,97 +203,19 @@ namespace TaoMod.NPCs.Shaoyang
 						npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * dashVelocity;
 					}
 				}
-
 				if (DashTimer > 0 && DashDuration == 0)
-					npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * stillVelocity;	
-				}
-		public void ExpertSpearAttack(){
-			ShootSpear++;
-			ExpertShootSpear++;
-			if (++ShootSpear % 480 == 0)
 				{
-					if (++ExpertShootSpear % 4 == 0)
+					npc.velocity = npc.DirectionTo(Main.player[npc.target].Center) * stillVelocity;
+				}
+				if(DoSpear % 360 == 0)
+				{
+					ShootSpear++;
+					if (++ShootSpear % 600 == 0)
 					{
 						NPC.NewNPC((int)npc.Center.X, (int)npc.position.Y, ModContent.NPCType<YangSpear>());
-						NPC.NewNPC((int)npc.Center.X + 50, (int)npc.position.Y - 53, ModContent.NPCType<YangSpear>());
-						NPC.NewNPC((int)npc.Center.X - 50, (int)npc.position.Y - 53, ModContent.NPCType<YangSpear>());
-
-						ExpertShootSpear = 0;
+						ShootSpear = 0;
 					}
-					ShootSpear = 0;
-				}
-		}
-		public void NormalSpearAttack(){
-			ShootSpear++;
-			if (++ShootSpear % 600 == 0)
-			{
-				NPC.NewNPC((int)npc.Center.X, (int)npc.position.Y, ModContent.NPCType<YangSpear>());
-				ShootSpear = 0;
-			}
-		}
-
-		public override void AI()
-		{
-			npc.rotation++;
-			npc.TargetClosest(true);
-			if (++AlphaTimer % 3 == 0 && npc.alpha > 40)
-			{
-				npc.alpha -= 5;
-				AlphaTimer = 0;
-			}
-
-			if (Main.expertMode)
-			{	
-				if(npc.life > 0){
-					AI_Timer++;
-					if(AI_Timer < 30){
-						AI_State = 0;
-					}
-					else if(AI_Timer > 30 && AI_Timer < 60){
-						AI_State = 1;
-					}
-					else if(AI_Timer > 60 && AI_Timer < 90){
-						AI_State = 2;
-					}
-					else if(AI_Timer > 90){
-						AI_Timer = 0;
-					}
-					if (AI_State == State_Dash) {
-						ExpertDashAttack();
-					}
-					else if (AI_State == State_Orb) {
-						ExpertOrbAttack();
-					}
-					else if (AI_State == State_Spear) {
-						ExpertSpearAttack();
-					}
-				}
-			}
-			else
-			{
-				if(npc.life > 0){
-					AI_Timer++;
-					if(AI_Timer < 30){
-						AI_State = 0;
-					}
-					else if(AI_Timer > 30 && AI_Timer < 60){
-						AI_State = 1;
-					}
-					else if(AI_Timer > 60 && AI_Timer < 90){
-						AI_State = 2;
-					}
-					else if(AI_Timer > 90){
-						AI_Timer = 0;
-					}
-					if (AI_State == State_Dash) {
-						NormalDashAttack();
-					}
-					else if (AI_State == State_Orb) {
-						NormalOrbAttack();
-					}
-					else if (AI_State == State_Spear) {
-						NormalSpearAttack();
-					}
+					DoSpear = 0;
 				}
 			}
 		}
